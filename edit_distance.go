28dd9d7c14a4ace85cda63a0df7686d5e97dd41f
guonaihong <guonaihong@qq.com>
2020-08-01 22:06:45 +0800

    first commit

---
+++ edit_distance.go
@@ -0,0 +1,10 @@
+package strsim
+
+type edit struct {
+}
+
+func (e *edit) Compare(s1, s2 string) {
+}
+
+func (e *edit) CompareRune(s1, s2 []rune) {
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/antlabs/strsim
+
+go 1.14
