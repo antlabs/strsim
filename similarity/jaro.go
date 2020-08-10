5ddf2e80677eddda2c356dea5cc7209ea6db7fd8
guonaihong <guonaihong@qq.com>
2020-08-10 20:46:57 +0800

    上传jaro粗步实现

--- similarity/jaro.go
+++ similarity/jaro.go
@@ -1,12 +1,61 @@
 package similarity
 
-import "unicode/utf8"
+import (
+	"fmt"
+	"math"
+	"unicode/utf8"
+)
 
-type Jaro struct{}
+type Jaro struct {
+	// test use
+	mw int
+	m  int
+	t  int
+}
 
 func (j *Jaro) CompareAscii(s1, s2 string) float64 {
+	return j.CompareUtf8(s1, s2)
 }
 
 func (j *Jaro) CompareUtf8(s1, s2 string) float64 {
-	mw := (utf8.RuneCountInString(s1)+utf8.RuneCountInString(s2))/2 - 1
+	mw := max(utf8.RuneCountInString(s1), utf8.RuneCountInString(s2))/2 - 1
+	m := 0
+
+	set := make(map[rune][]int, len(s1)/3)
+	l1 := 0
+	for k, c := range s1 {
+		set[c] = append(set[c], k)
+		l1++
+	}
+
+	t := 0
+	l2 := 0
+
+	best := 0
+	for s2Index, c := range s2 {
+		indexs, ok := set[c]
+		l2++
+		if !ok {
+			continue
+		}
+
+		for _, i := range indexs {
+			if math.Abs(float64(s2Index-i)) <= float64(mw) {
+				m++
+				if i < best {
+					t++
+					fmt.Printf("--->%c:s2Index(%d):i(%d), s2Index-i(%d), mw:%d\n", c, s2Index, i, s2Index-i, mw)
+				}
+				best = i
+
+				break
+			}
+		}
+	}
+
+	m2 := float64(m)
+	j.mw = mw
+	j.m = m
+	j.t = t
+	return 1.0 / 3.0 * (m2/float64(l1) + m2/float64(l2) + (m2-float64(t)/2.0)/m2)
 }
---
+++ similarity/jaro_test.go
@@ -0,0 +1,26 @@
+package similarity
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Jaro_CompareAscii(t *testing.T) {
+	j := &Jaro{}
+
+	for k, v := range []testOneCase{
+
+		{s1: "bacde", s2: "abed", cost: 0.6722222222222222},
+		{s1: "MARTHA", s2: "MARHTA", cost: 0.9444444444444444},
+		{s1: "DIXON", s2: "DICKSONX", cost: 0.7666666666666666},
+
+		{s1: "JELLYFISH", s2: "SMELLYFISH", cost: 0.8962962962962964},
+	} {
+		m := fmt.Sprintf("error case:%d", k)
+		assert.Equal(t, j.CompareAscii(v.s1, v.s2), v.cost, m)
+		fmt.Printf("m:%d, t:%d\n", j.m, j.t)
+	}
+
+}
